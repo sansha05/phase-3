1428722cbc9816e7bd9b983b119b7448ed09f5ee
sansha05 <ss05311999@gmail.com>
2022-03-28 19:33:23 +0530

    2.4 done

---
+++ 2.4/src/main/java/com/custom/exception/controller/EProductExceptionController.java
@@ -0,0 +1,20 @@
+package com.custom.exception.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.custom.exception.ex.ProductNotFoundException;
+
+
+
+@ControllerAdvice
+public class EProductExceptionController {
+
+        @ExceptionHandler(value = ProductNotFoundException.class)
+           public ResponseEntity<Object> exception(ProductNotFoundException exception) { 
+              return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+           }
+}
+
---
+++ 2.4/src/main/java/com/custom/exception/ex/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.custom.exception.ex;
+
+
+
+public class ProductNotFoundException extends RuntimeException {
+           private static final long serialVersionUID = 1L;
+
+}
